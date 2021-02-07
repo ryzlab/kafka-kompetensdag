1ce18ee32130a924bf52f043cd0b5de457fa2cf4
Mats Johannesson <ryzlab@ryz.se>
2021-02-07 11:59:02 +0100

    Fixat lite

---
+++ doc/Kafka-JUG.mm
@@ -0,0 +1,121 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Kafka JUG" FOLDED="false" ID="ID_1867849874" CREATED="1559121397455" MODIFIED="1559121410166" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Kafka" POSITION="right" ID="ID_90563705" CREATED="1559121414312" MODIFIED="1559121424199">
+<edge COLOR="#ff0000"/>
+<node TEXT="Motivation" ID="ID_1928082085" CREATED="1559121424317" MODIFIED="1559121426852"/>
+<node TEXT="OSS" ID="ID_90184716" CREATED="1559121429371" MODIFIED="1559121431666"/>
+</node>
+<node TEXT="Mesages" POSITION="right" ID="ID_1653724800" CREATED="1559121434067" MODIFIED="1559121436393">
+<edge COLOR="#0000ff"/>
+</node>
+<node TEXT="Topics" POSITION="right" ID="ID_345940434" CREATED="1559121440008" MODIFIED="1559121443564">
+<edge COLOR="#00ff00"/>
+<node TEXT="Partitions" ID="ID_1887184146" CREATED="1559121445617" MODIFIED="1559121448844"/>
+<node TEXT="Durable" ID="ID_985821964" CREATED="1559121451438" MODIFIED="1559121453344"/>
+<node TEXT="Ordered" ID="ID_59976030" CREATED="1559121454692" MODIFIED="1559121456523"/>
+</node>
+<node TEXT="Demo" POSITION="left" ID="ID_1592461012" CREATED="1559121808889" MODIFIED="1559121810795">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Demo Cluster" ID="ID_657121997" CREATED="1559121824413" MODIFIED="1559121829380"/>
+<node TEXT="Console producer/consumer" ID="ID_509630034" CREATED="1559121835529" MODIFIED="1559121842685"/>
+<node TEXT="Streams Producer/Consumer" ID="ID_1234085137" CREATED="1559121843209" MODIFIED="1559121850665">
+<node TEXT="Topic three partitions" ID="ID_1392208487" CREATED="1559121875009" MODIFIED="1559121880877"/>
+<node TEXT="Two Consumers" ID="ID_1095088704" CREATED="1559121861780" MODIFIED="1559121873916"/>
+<node TEXT="One producer" ID="ID_540052601" CREATED="1559121882840" MODIFIED="1559121885872"/>
+</node>
+<node TEXT="Knyta ihop" ID="ID_278442253" CREATED="1559122999218" MODIFIED="1559123010786">
+<node TEXT="Kan kafka vara n&#xe5;got?" ID="ID_26684576" CREATED="1559123011158" MODIFIED="1559123030960"/>
+<node TEXT="N&#xe5;got nytt ni l&#xe4;rt er?" ID="ID_979944671" CREATED="1559123031723" MODIFIED="1559123039705"/>
+<node TEXT="&#xc4;ndrat uppfattning?" ID="ID_865742802" CREATED="1559123040438" MODIFIED="1559123044415"/>
+<node TEXT="Undrar n&#xe5;got mer?" ID="ID_1143685189" CREATED="1559123045584" MODIFIED="1559123049831"/>
+<node TEXT="Det finns mycket mer att s&#xe4;ga, fr&#xe5;ga g&#xe4;rna" ID="ID_1980247314" CREATED="1559123050984" MODIFIED="1559123065009"/>
+<node TEXT="TACK!!!" ID="ID_1082164648" CREATED="1559123065729" MODIFIED="1559123067381"/>
+</node>
+</node>
+<node TEXT="Kafka-Manager" POSITION="left" ID="ID_1343892380" CREATED="1559121796152" MODIFIED="1559121801240">
+<edge COLOR="#007c7c"/>
+</node>
+<node TEXT="ZooKeeper" POSITION="left" ID="ID_1834904395" CREATED="1559121510233" MODIFIED="1559121513354">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="Producers" POSITION="left" ID="ID_334392724" CREATED="1559121506813" MODIFIED="1559121508419">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Consumers" POSITION="left" ID="ID_1058514562" CREATED="1559121473294" MODIFIED="1559121482202">
+<edge COLOR="#7c0000"/>
+<node TEXT="Groups" ID="ID_214657961" CREATED="1559121482530" MODIFIED="1559121485771"/>
+<node TEXT="Three API:s" ID="ID_581380215" CREATED="1559121486158" MODIFIED="1559121496029">
+<node TEXT="Processor" ID="ID_1218568615" CREATED="1559121496988" MODIFIED="1559121500319"/>
+<node TEXT="Stream" ID="ID_1076804915" CREATED="1559121500557" MODIFIED="1559121501715"/>
+<node TEXT="KSQL&#xa8;" ID="ID_1756619309" CREATED="1559121501863" MODIFIED="1559121503772"/>
+</node>
+</node>
+<node TEXT="Brokers" POSITION="left" ID="ID_1425394657" CREATED="1559121461181" MODIFIED="1559121464510">
+<edge COLOR="#ff00ff"/>
+</node>
+</node>
+</map>
--- exercises/src/test/java/se/ryz/kafka/demo/PollTimeout.java
+++ exercises/src/test/java/se/ryz/kafka/demo/PollTimeout.java
@@ -1,4 +1,31 @@
 package se.ryz.kafka.demo;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.junit.Test;
+import se.ryz.kafka.demo.util.Common;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Properties;
+
 public class PollTimeout {
+
+    @Test
+    public void pollLongSleep() throws InterruptedException {
+        String topicName = "slow-poll-topic";
+        String clientId = "slowClient";
+        String groupId = "slowGroup";
+        Common common = new Common();
+        Properties props = common.createConsumerConfig(groupId, clientId);
+        // Create the consumer and subscribe
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList(topicName));
+        // Poll for records and process them
+        ConsumerRecords<String, String> consumerRecords = consumer.poll(Duration.ofSeconds(1));
+        System.out.println("Sleeping forever");
+        Thread.sleep(3600000*24);
+    }
 }
